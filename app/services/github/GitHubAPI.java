d5286a3a745e7ac658dcd3901f273d21a641b076
Smit Pateliya <39058954+SMITPATELIYA18@users.noreply.github.com>
2021-11-28 17:34:12 -0500

    Bug Fixes

--- app/services/github/GitHubAPI.java
+++ app/services/github/GitHubAPI.java
@@ -5,14 +5,10 @@ import java.util.concurrent.CompletionStage;
 import models.IssueModel;
 
 public interface GitHubAPI {
-<<<<<<< Updated upstream
-	public CompletionStage<IssueModel> getRepositoryIssue(String repoFullName);
-=======
-	CompletionStage<SearchRepository> getRepositoryFromSearchBar(String query) throws Exception;
+CompletionStage<SearchRepository> getRepositoryFromSearchBar(String query) throws Exception;
 	CompletionStage<JsonNode> getUserProfileByUsername(String username) throws Exception;
 	CompletionStage<JsonNode> getUserRepositories(String username) throws Exception;
 	CompletionStage<JsonNode> getRepositoryProfile(String username, String repositoryName) throws Exception;
-	CompletionStage<JsonNode> getRepositoryIssue(String repoFullName);
+	CompletionStage<IssueModel> getRepositoryIssue(String repoFullName);
 	CompletionStage<SearchRepository> getTopicRepository(String topic) throws Exception;
->>>>>>> Stashed changes
 }
